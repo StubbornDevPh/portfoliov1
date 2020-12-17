1dfb8ad9894f2ee64e2d2bb09b013d46986fa3a9
Al Francis Gabriel Bolima <alfrancisgabriel@gmail.com>
2020-12-17 13:58:05 +0800

    feat:added logo and minor modifications

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const About = () => {
+	return (
+		<div>
+			<Head>
+				<title>Gabcodes - About</title>
+			</Head>
+			<div>{/* Content goes here */}</div>
+		</div>
+	);
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from "next/head";
 
-export default Home = () => {
+const Home = () => {
 	return (
 		<div>
 			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
+				<title>Gabcodes</title>
+				<link rel="icon" href="/favicon.png" />
 			</Head>
+			<div>
+				{/* Content goes here */}
+				<img src="/gabcodes.png" width="200px" height="auto"></img>
+			</div>
 		</div>
 	);
 };
+
+export default Home;
